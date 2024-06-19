af130c66b5ac6d7792358fdf2a97d5e396a288ba
BKM23 <brian.mutungi@strathmore.edu>
2024-06-19 19:29:32 +0300

    eighteenth commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Jersey Store</title>
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <script src="js/bootstrap.js"></script>
+        <style>
+            .active-link {
+                background-color: purple;
+                color: black;
+                padding: 15px;
+            }
+
+            /* hover only on the nav items */
+            .nav-item:hover {
+                background-color: white;
+                color: black;
+                padding: 8px;
+            }
+        </style>
+    </head>
+    <body>
+        
+        <?php include_once("templates/nav.php")?>
+        <?php include_once("templates/carousel.php");?>            
+        <?php include_once("templates/indbody.php");?>     
+    </body>
+
+    
+</html>
---
+++ template/nav.php
@@ -0,0 +1,28 @@
+<section class="row"> 
+                <div class="col-md-12">
+                    <nav class=" fixed-top navbar navbar-expand-lg bg-info navbar-light p-4">
+                        <a href="#" class="navbar-brand">Jersey Store <img src="images/jersey.png" height="40"
+                                width="40" alt=""></a>
+                        
+                        <button type="button" class="navbar-toggler" data-bs-toggle="collapse"
+                            data-bs-target="#navbarcollapse">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarcollapse">
+                            <div class="navbar-nav">
+                                <a href="index.html" class="nav-item nav-link">Home</a>  
+                                <a href="clubjerseys.html" class="nav-item nav-link">Club Jerseys</a> 
+                                <a href="internationaljerseys.html" class="nav-item nav-link">International Jerseys</a>  
+                                <a href="retrojerseys.html" class="nav-item nav-link">Retro Jerseys</a>  
+                                <a href="basketballjerseys.html" class="nav-item nav-link">Basketball Jerseys</a> 
+                                <a href="specialjerseys.html" class="nav-item nav-link">Special Jerseys</a>
+                                <a href="registration.html" class="nav-item nav-link">Sign up</a>  
+                                <a href="subscription.html" class="nav-item nav-link">Subscribe</a>
+
+                            </div>
+                            
+                        </div>
+
+                    </nav>
+                </div>
+            </section><br><br><br><br>
