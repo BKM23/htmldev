2e064252a58c09aaf01777a7dd4efc875512c7ca
BKM23 <brian.mutungi@strathmore.edu>
2024-07-10 21:32:12 +0300

    converting to php

--- basketballjerseys.php
+++ basketballjerseys.php
@@ -10,6 +10,109 @@
 <?php include_once("templates/nav.php")?>
 
     <h2>Basketball jerseys</h2>
-    
+    <section class="row text-center">
+            <div class="col-md-2">
+                <div class="card">
+                    <img src="images/lakers yellow.png" class="card-img-top img-fluid">
+                    <!-- card body -->
+                    <div class="card-body">    
+                    </div>
+                    <!-- end of body -->
+                    <div class="card-footer">
+                        <h6 class="text-primary">Los Angeles Lakers Yellow</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card">
+                    <img src="images/lakers black.jpeg" class="card-img-top img-fluid">
+                    <div class="card-body">
+                        <div class="card-title">
+                            <h3 class="text-info"></h3>
+                        </div>
+                        <div class="card-text text-capitalize text-secondary">
+                            <p></p>
+                        </div>
+                        
+                    </div>
+                    <!-- end of body -->
+                    <div class="card-footer">
+                        <h6 class="text-primary">Los Angeles Lakers Black</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card">
+                    <img src="images/lakers purple.webp" class="card-img-top img-fluid">
+                    <div class="card-body">
+                        <div class="card-title">
+                            <h3 class="text-primary"></h3>
+                        </div>
+                        <div class="card-text text-capitalize text-secondary">
+                            <p></p>
+                        </div>
+                        
+                    </div>
+                    <!-- end of body -->
+                    <div class="card-footer">
+                        <h6 class="text-primary">Los Angeles Lakers Purple</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card">
+                    <img src="images/golden state black.jpeg" class="card-img-top img-fluid">
+                    <div class="card-body">
+                        <div class="card-title">
+                            <h6 class="text-primary"></h6>
+                        </div>
+                        <div class="card-text text-capitalize text-secondary">
+                            
+                        </div>
+                        
+                    </div>
+                    <!-- end of body -->
+                    <div class="card-footer">
+                        <h6 class="text-primary">Golden State Warriors Black</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card">
+                    <img src="images/golden state blue.png" class="card-img-top img-fluid">
+                    <div class="card-body">
+                        <div class="card-title">
+                            <h3 class="text-primary"></h3>
+                        </div>
+                        <div class="card-text text-capitalize text-secondary">
+                            <p></p>
+                        </div>
+                        
+                    </div>
+                    <!-- end of body -->
+                    <div class="card-footer">
+                        <h6 class="text-primary">Golden State Warriors Blue</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card">
+                    <img src="images/golden state special.jpeg" class="card-img-top img-fluid">
+                    <div class="card-body">
+                        <div class="card-title">
+                            <h3 class="text-primary"></h3>
+                        </div>
+                        <div class="card-text text-capitalize text-secondary">
+                            <p></p>
+                        </div>
+                        
+                    </div>
+                    <!-- end of body -->
+                    <div class="card-footer">
+                        <h6 class="text-primary">Golden State Warriors Special Edition</h6>
+                    </div>
+                </div>
+            </div>
+        </section> 
 </body>
 </html>
